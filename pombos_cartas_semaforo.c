96781d5e3872e899104615bb6802e695548ef285
Ricardo Santos <180027263@aluno.unb.br>
2022-03-29 20:07:25 -0300

    Carrier pigeon problem with semaphores

---
+++ pombos_cartas_semaforo.c
@@ -0,0 +1,122 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unistd.h"
+
+#define N 10  // número de usuários
+
+#define CARTAS 20  // quantidade de cartas na mochila
+
+sem_t *mochila;
+sem_t *pombo;
+pthread_mutex_t mochila_pombo = PTHREAD_MUTEX_INITIALIZER;
+
+void *f_usuario(void *arg);
+void *f_pombo(void *arg);
+
+int main(int argc, char **argv) {
+    int i;
+
+    sem_unlink("mochila");
+    mochila = sem_open("mochila", O_CREAT, 0777, 20);
+    if (mochila == SEM_FAILED) {
+        fprintf(stderr, "%s\n", "ERROR creating semaphore mochila");
+        exit(EXIT_FAILURE);
+    }
+    sem_unlink("pombo");
+    pombo = sem_open("pombo", O_CREAT, 0777, 0);
+    if (pombo == SEM_FAILED) {
+        fprintf(stderr, "%s\n", "ERROR creating semaphore pombo");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_t usuario[N];
+    int *id;
+    for (i = 0; i < N; i++) {
+        id = (int *)malloc(sizeof(int));
+        *id = i;
+        pthread_create(&(usuario[i]), NULL, f_usuario, (void *)(id));
+    }
+    pthread_t pombo;
+    id = (int *)malloc(sizeof(int));
+    *id = 0;
+    pthread_create(&(pombo), NULL, f_pombo, (void *)(id));
+
+    pthread_join(pombo, NULL);
+}
+
+int n_cartas = 0;
+
+void *f_pombo(void *arg) {
+    time_t rawtime;
+    struct tm *timeinfo;
+    struct tm buf;
+    char str[30];
+
+    while (1) {
+        // Inicialmente está em A, aguardar/dorme a mochila ficar cheia (20 cartas)
+        time(&rawtime);
+        timeinfo = localtime_r(&rawtime, &buf);
+        asctime_r(timeinfo, str);
+        str[24] = '\0';
+
+        printf("[%s] Pombo esperando em A\n", str);
+        sem_wait(pombo);
+
+        // Leva as cartas para B e volta para A
+        pthread_mutex_lock(&mochila_pombo);
+
+        time(&rawtime);
+        timeinfo = localtime_r(&rawtime, &buf);
+        asctime_r(timeinfo, str);
+        str[24] = '\0';
+        printf("[%s] Pombo em B\n", str);
+
+        sleep(3);
+        n_cartas = 0;
+
+        // Acordar os usuários
+        for (int i = 0; i < CARTAS; i++) {
+            sem_post(mochila);
+        }
+        pthread_mutex_unlock(&mochila_pombo);
+    }
+}
+
+void *f_usuario(void *arg) {
+    int id = *(int *)arg;
+    time_t rawtime;
+    struct tm *timeinfo;
+    struct tm buf;
+    char str[30];
+    while (1) {
+        // Escreve uma carta
+        time(&rawtime);
+        timeinfo = localtime_r(&rawtime, &buf);
+        asctime_r(timeinfo, str);
+        str[24] = '\0';
+        printf("[%s] Usuário %d escrevendo carta\n", str, id);
+        sleep(rand() % 7);
+
+        // Caso o pombo não esteja em A ou a mochila estiver cheia, então dorme
+        sem_wait(mochila);
+
+        // Posta sua carta na mochila do pombo
+        pthread_mutex_lock(&mochila_pombo);
+        n_cartas++;
+        time(&rawtime);
+        timeinfo = localtime_r(&rawtime, &buf);
+        asctime_r(timeinfo, str);
+        str[24] = '\0';
+        printf("[%s] Usuário %d escreveu carta: %d\n", str, id, n_cartas);
+
+        if (n_cartas == CARTAS) {
+            // Caso a mochila fique cheia, acorda o ṕombo
+            sem_post(pombo);
+            printf("Usuário %d encheu mochila\n", id);
+        }
+        pthread_mutex_unlock(&mochila_pombo);
+    }
+}
